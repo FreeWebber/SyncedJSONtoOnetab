bd74eb7b0591b42311ed744656c8cf83eb6aca5a
Alex Nevi <acerlmt@gmail.com>
2023-08-21 17:40:50 +0700

    impfixs

--- SyncedJSONtoOnetab.js
+++ SyncedJSONtoOnetab.js
@@ -6,10 +6,19 @@ cl = console.log; //cl($('textarea[name=tabs]'));
 $('.convert_btn').on('click', function(){ convert($('textarea[name=tabs]').val()); });
 $('textarea[name=tabs]').on('change input', function(){ convert(this.value); });
 
-function convert(val){
-    var data = JSON.parse(val);
-    var $otta = $('textarea[name=ot]');
+var $otta = $('textarea[name=ot]');
     $otta.html('');
+
+function convert(val){
+    $otta.html('');
+    if(!val) return;
+    try {
+        var data = JSON.parse(val);
+    } catch(e) {
+        cl(e);
+        $otta.html('cant convert');
+        return;
+    }
     $.each(data.tabs, function(k, v){
         $otta.append(this.urlHistory +' | '+ this.title +"\n"); //$otta.append(k+1 +'. ' +this.urlHistory +' | '+ this.title +"\n");
     });
